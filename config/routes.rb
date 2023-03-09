2600298c33c6804f8b786bf1fc7077b4c413eb92
ekaterina <ekkraskovskaya@gmail.com>
2023-03-09 13:31:34 +0300

    added devise and posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+
+  devise_scope :user do
+  get '/users' => 'devise/registration#new'
+  get '/users/password' =>  'devise/password#new'
+  get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+  resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "posts#index"
 end
